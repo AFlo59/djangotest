1dec5f71088e3b42625f4bff8d1b30a39284ea9f
AFlo59 <etudessup59230@gmail.com>
2024-02-20 11:03:07 +0100

    up

--- functionalities/urls.py
+++ functionalities/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from functionalities import views
 
 urlpatterns = [
-    path('list/', views.FunctionalitiesListView.as_view(), name="func-list"),
-    path('<int:pk>/', views.FunctionalitiesDetailView.as_view(), name="func-detail"),
-    path('signup/', views.SignupView.as_view(), name="signup")
+    path('', views.FunctionalitiesListView.as_view(), name="funct_list"),
+    path('<int:pk>/', views.FunctionalitiesDetailView.as_view(), name='funct_detail'),
+    path('signup/', views.SignupView.as_view(), name='signup')
 ]
